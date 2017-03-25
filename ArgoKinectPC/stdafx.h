d1fd885e367e7f5ef208d1c537c5b525906d0d36
Yolo Jolo <jolosimeon@users.noreply.github.com>
2017-03-26 00:12:00 +0800

    Init

---
+++ ArgoKinectPC/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+#define NOMINMAX
+#include <Windows.h>
+#undef NOMINMAX
+#include <Kinect.h>
+#include <pcl\visualization\cloud_viewer.h>
+#include <pcl\common\projection_matrix.h>
+#include <pcl\point_cloud.h>
+#include <iostream>
