18bdfff283566c8e7b285152ec7b187f7593693c
kingstonkoa <kingston_koa@yahoo.com>
2017-06-06 01:16:02 +0800

    error RGBA

--- ArgoKinectPC/Segmenter.cpp
+++ ArgoKinectPC/Segmenter.cpp
@@ -18,7 +18,7 @@ Segmenter::~Segmenter() {
 }
 
 
-void Segmenter::segmentCloud(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr input, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr output) {
+void Segmenter::segmentCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input, pcl::PointCloud<pcl::PointXYZRGB>::Ptr output) {
 	//initialize data so it can be used in other functions, point class ptrs to actual ptrs in main
 	this->input = input; //input is what functions use
 	this->output = output; //output is result of the function
@@ -38,8 +38,8 @@ void Segmenter::segmentCloud(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr input, pcl:
 }
 
 void Segmenter::lowerVisibleArea() {
-	pcl::PointCloud<pcl::PointXYZRGBA>::Ptr  smolCloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
-	pcl::PassThrough<pcl::PointXYZRGBA> pass;
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr  smolCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PassThrough<pcl::PointXYZRGB> pass;
 	pass.setInputCloud(input);
 	pass.setFilterFieldName("z");
 	pass.setFilterLimits(0.3, 0.9);
@@ -55,7 +55,7 @@ void Segmenter::segmentPlane() {
 	//Remove the plane (floor/table)
 	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-	pcl::SACSegmentation<pcl::PointXYZRGBA> seg;
+	pcl::SACSegmentation<pcl::PointXYZRGB> seg;
 
 	seg.setOptimizeCoefficients(true);
 	seg.setModelType(pcl::SACMODEL_PLANE);
@@ -63,7 +63,7 @@ void Segmenter::segmentPlane() {
 	seg.setMaxIterations(100);
 	seg.setDistanceThreshold(0.02);
 
-	pcl::ExtractIndices<pcl::PointXYZRGBA> extract;
+	pcl::ExtractIndices<pcl::PointXYZRGB> extract;
 	int i = 0, nr_points = (int)input->points.size();
 
 	// Form the largest planar component from the cloud
